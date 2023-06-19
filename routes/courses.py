e615ccb1f2579d8a2b7a02acfdc918a6225cf5d5
Durga Gadiraju <durga@skyhive.io>
2023-06-19 11:31:40 +0530

    Refactored code

--- routes/courses.py
+++ routes/courses.py
@@ -24,36 +24,33 @@ def course():
         course_id = request.args.get('course_id')
         if course_id:
             course = Course.query.get(course_id)
-            return render_template('course_detail.html', course=course)
-        else:
-            return render_template('course_form.html', course=None)
-    elif request.method == 'POST':
-        course_id = request.args.get('course_id')
-        if course_id:
-            course = Course.query.get(course_id)
-            form_action = request.form['action']
+            form_action = request.args.get('action')
             if form_action == 'edit':
                 return render_template('course_form.html', course=course)
             elif form_action == 'delete':
                 db.session.delete(course)
                 db.session.commit()
                 return redirect(url_for('courses'))
+            return render_template('course_detail.html', course=course)
         else:
-            course_id = request.form['course_id']
-            course_name = request.form['course_name']
-            course_author = request.form['course_author']
-            course_endpoint = request.form['course_endpoint']
-            if course_id:
-                course = Course.query.get(course_id)
-                course.course_name = course_name
-                course.course_author = course_author
-                course.course_endpoint = course_endpoint
-            else:
-                course = Course(
-                    course_name=course_name, 
-                    course_author=course_author, 
-                    course_endpoint=course_endpoint
-                )
-                db.session.add(course)
-            db.session.commit()
-            return redirect(url_for('courses'))
+            return render_template('course_form.html', course=None)
+    elif request.method == 'POST':
+        course_id = request.args.get('course_id')
+        course_id = request.form['course_id']
+        course_name = request.form['course_name']
+        course_author = request.form['course_author']
+        course_endpoint = request.form['course_endpoint']
+        if course_id:
+            course = Course.query.get(course_id)
+            course.course_name = course_name
+            course.course_author = course_author
+            course.course_endpoint = course_endpoint
+        else:
+            course = Course(
+                course_name=course_name, 
+                course_author=course_author, 
+                course_endpoint=course_endpoint
+            )
+            db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('courses'))
